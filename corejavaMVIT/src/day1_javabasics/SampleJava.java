0f3cdd5109b5e9810f25d2be31ad4b7df69d536a
brsm1 <brsm1@DESKTOP-C6P384P.mshome.net>
2024-03-05 15:43:57 +0530

    core java practical programs

---
+++ corejavaMVIT/src/day1_javabasics/SampleJava.java
@@ -0,0 +1,11 @@
+package day1_javabasics;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to JFS class...");
+		System.out.print("by sakthi");
+
+	}
+
+}
---
+++ corejavaMVIT/src/day1_javabasics/SampleJava3.java
@@ -0,0 +1,18 @@
+package day1_javabasics;
+import java.util.*;
+public class SampleJava3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		
+		System.out.println("enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+
+	}
+
+}
